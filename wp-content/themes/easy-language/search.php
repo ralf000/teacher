7de79dbeab36f827bdc8aaf90f0723dd2d93ceb1
ralf <ralf000@yandex.ru>
2017-01-11 18:33:17 +0300

    сокрытие страниц сложений корректировка поиска

--- wp-content/themes/easy-language/search.php
+++ wp-content/themes/easy-language/search.php
@@ -3,26 +3,29 @@
     <!-- Blog Content
      ================================================== -->
     <div class="row">
-
         <!-- Blog Posts
         ================================================== -->
         <div class="span8 blog">
-
             <?php if (have_posts()) : ?>
                 <?php while (have_posts()) : the_post(); ?>
+                    <?php if(strpos(get_the_permalink(), get_site_url() . '/slide') !== false) continue; ?>
                     <!-- Category Post -->
                     <article>
-                        <h3 class="title-bg"><a href="<?php the_permalink()?>"><?php the_title()?></a></h3>
+                        <h3 class="title-bg"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
                         <div class="post-summary">
-                            <?php the_excerpt()?>
+                            <?php the_excerpt() ?>
                             <div class="post-summary-footer">
-                                <button class="btn btn-small btn-inverse" onclick="location.href='<?php the_permalink()?>'" type="button">Read more</button>
+                                <button class="btn btn-small btn-inverse"
+                                        onclick="location.href='<?php the_permalink() ?>'" type="button">Read more
+                                </button>
                                 <ul class="post-data">
-                                    <li><i class="icon-calendar"></i> <?php the_time('d/m/y')?></li>
-                                    <li><i class="icon-user"></i> <a href="<?php the_author_link() ?>"><?php the_author()?></a></li>
-                                    <li><i class="icon-comment"></i> <a href="<?php comment_link() ?>"><?php comments_number() ?></a></li>
+                                    <li><i class="icon-calendar"></i> <?php the_time('d/m/y') ?></li>
+                                    <li><i class="icon-user"></i> <a
+                                            href="<?php the_author_link() ?>"><?php the_author() ?></a></li>
+                                    <li><i class="icon-comment"></i> <a
+                                            href="<?php comment_link() ?>"><?php comments_number() ?></a></li>
                                     <li><i class="icon-tags"></i>
-                                        <?= getTags(get_the_tag_list())?>
+                                        <?= getTags(get_the_tag_list()) ?>
                                     </li>
                                 </ul>
                             </div>
